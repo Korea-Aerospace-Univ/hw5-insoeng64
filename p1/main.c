fcf3551b9628cb6a9c9884d27d7e6f8aded07562
insoeng64 <108975160+insoeng64@users.noreply.github.com>
2026-04-29 15:55:25 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int T;
+    int a = 900;
+    int b = 750;
+    int c = 200;
+    scanf_s("%d", &T);
+    for (int i = 1; i < T / a; i++)
+    {
+        for (int y = 2; y < T / b; y = y + 2)
+        {
+            for (int z = 1; z < T / c; z++) {
+                if ((i * a + y * b + z * c) == T) {
+                    if (z < i || z < y) {
+                        printf("%d %d %d\n", i, y, z);
+                    }
+                }
+            }
+        }
+    }
+    printf("none");
+    return 0;
+}
